b77ff4a7a4907c737c3a7ffab1cabd70c84de664
Melissa Brewer <melramirez727@gmail.com>
2021-09-20 16:41:55 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+// import Header from './components/Header';
 // import Footer from './components/Footer';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>APP</h1>
-      <Header />
+      <h1>Hello</h1>
   
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-// import About from './components/About/index.js';
-// import Contact from '../components/Contact';
-// import Project from '../components/Project';
-// import Resume from '../components/Resume';
+// import React from "react";
+// // import About from './components/About/index.js';
+// // import Contact from '../components/Contact';
+// // import Project from '../components/Project';
+// // import Resume from '../components/Resume';
 
 
-export default function Header() {
-    return (
-        <div>
-         <h1>hello</h1> 
-        </div>
-    )
-}
+// export default function Header() {
+//     return (
+//         <div>
+//          <h1>hello</h1> 
+//         </div>
+//     )
+// }
